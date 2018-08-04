a5eb21c4f25d92907e892794c2792292fc88c75a
Alyson Takai <shtakai@gmail.com>
2018-08-04 16:54:40 +0700

    Modified createboard/with bomb

--- __test__/test.test.js
+++ __test__/test.test.js
@@ -1,4 +1,4 @@
-const printBoard = require('../src/minesweeper')
+const {printBoard, generatePlayerBoard, generateBombBoard} = require('../src/minesweeper')
 
 describe('printBoard', () => {
   test('simple board', () => {
@@ -19,4 +19,52 @@ describe('printBoard', () => {
     )
   })
 
+})
+
+describe('generatePlayerBoard', () => {
+  test('3x2 board', () => {
+    const board = generatePlayerBoard(3, 2)
+    const expectedBoard = [
+      [ ' ', ' ' ],
+      [ ' ', ' ' ],
+      [ ' ', ' ' ],
+    ]
+    expect(board).toEqual(expectedBoard)
+  })
+
+  test('2x3 board', () => {
+    const board = generatePlayerBoard(2, 3)
+    const expectedBoard = [
+      [ ' ', ' ', ' ' ],
+      [ ' ', ' ', ' ' ],
+    ]
+    expect(board).toEqual(expectedBoard)
+  })
+})
+
+describe('generateBombBoard', () => {
+  test('3x2 board without bombs', () => {
+    const board = generateBombBoard(3, 2, 0)
+    const expectedBoard = [
+      [ null, null ],
+      [ null, null ],
+      [ null, null ],
+    ]
+    expect(board).toEqual(expectedBoard)
+  })
+
+  test('2x3 board without bombs', () => {
+    const board = generateBombBoard(2, 3, 0)
+    const expectedBoard = [
+      [ null, null, null ],
+      [ null, null, null ],
+    ]
+    expect(board).toEqual(expectedBoard)
+  })
+
+  test('10x10 board with 10 bombs', () => {
+    const board = generateBombBoard(10, 10, 10)
+    const flattenBoard = Array.prototype.concat.apply([], board)
+    expect(flattenBoard.filter(el => el === 'B').length).toEqual(10)
+  })
 })
--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,10 +1,55 @@
-const printBoard = board => {
-  let printBoard = 'Current Board:\n'
-  for (let col of board) {
-    printBoard += col.join(' | ')
-    printBoard += '\n'
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  let board = []
+  for (let i = 0; i < numberOfRows; i++) {
+    let row = []
+    for (let j = 0; j < numberOfColumns; j++) {
+      row.push(' ')
+    }
+    board.push(row)
   }
-  return printBoard
+  return board
 }
 
-module.exports = printBoard
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs = 0) => {
+  let numberOfBombsPlaced = 0
+  let board = []
+  for (let i = 0; i < numberOfRows; i++) {
+    let row = []
+    for (let j = 0; j < numberOfColumns; j++) {
+      row.push(null)
+    }
+    board.push(row)
+  }
+
+  // Add bombs
+  while (numberOfBombsPlaced < numberOfBombs) {
+    let randomRowIndex = parseInt(Math.random() * numberOfRows)
+    let randomColumnIndex = parseInt(Math.random() * numberOfColumns)
+    // inclease counter if bomb can be set.
+    if (board[randomRowIndex][randomColumnIndex] === null) {
+      board[randomRowIndex][randomColumnIndex] = 'B'
+      numberOfBombsPlaced++
+    }
+  }
+
+  return board
+  }
+
+const printBoard = board => {
+  let outputBoard = 'Current Board:\n'
+  for (let col of board) {
+    outputBoard += col.join(' | ')
+    outputBoard += '\n'
+  }
+  return outputBoard
+}
+
+module.exports = {
+  printBoard,
+  generatePlayerBoard,
+  generateBombBoard,
+}
+
+// console.log(generatePlayerBoard(2,4))
+// console.log(printBoard(generatePlayerBoard(2,4)))
+// console.log(generateBombBoard(3,4,2))
