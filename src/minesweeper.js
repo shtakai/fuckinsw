ca6a9bd4ea4edde74c218dd33446705f33dcf320
Alyson Takai <shtakai@gmail.com>
2018-08-06 21:03:05 +0700

    Add number of bombs neighborhood

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -35,6 +35,39 @@ const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs = 0) => 
   return board
 }
 
+const getNumberOfNeighborBombs = (bombBoard, rowIndex, columnIndex) => {
+  const neighborOffsets = [
+    [-1, -1],
+    [-1,  0],
+    [-1,  1],
+    [ 0, -1],
+    [ 0,  1],
+    [ 1, -1],
+    [ 1,  0],
+    [ 1,  1],
+  ]
+  const numberOfRows = bombBoard.length
+  const numberOfColumns = bombBoard[0].length
+
+  let numberOfBombs = 0
+
+  neighborOffsets.forEach(offset => {
+    const neighborRowIndex = rowIndex + offset[0]
+    const neighborColumnIndex = columnIndex + offset[1]
+    if (
+      neighborRowIndex >= 0 &&
+      neighborRowIndex < numberOfRows &&
+      neighborColumnIndex >= 0 &&
+      neighborColumnIndex < numberOfColumns
+    ) {
+      if (bombBoard[neighborRowIndex][neighborColumnIndex] === 'B') {
+        numberOfBombs++
+      }
+    }
+  })
+  return numberOfBombs
+}
+
 const printBoard = board => {
   return board.map(row => row.join(' | ')).join('\n')
 }
